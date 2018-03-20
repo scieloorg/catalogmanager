317d16a4d880d631cd69f583879f31673e3014c6
Patricia Morimoto <patriciamorimoto@Patricias-MacBook-Pro.local>
2018-03-20 16:08:09 -0300

    Alteração ao comentário do PR.

--- catalog_persistence/models.py
+++ catalog_persistence/models.py
@@ -40,6 +40,14 @@ class DocumentRecord(metaclass=abc.ABCMeta):
     def created_date(self, created_date):
         self._created_date = created_date
 
+    def input(self):
+        return {
+            'document_id': self.document_id,
+            'document_type': self.document_type.value,
+            'content': self.content,
+            'created_date': self.created_date,
+        }
+
     def output(self):
         return self
 
@@ -49,11 +57,3 @@ class ArticleRecord(DocumentRecord):
     def __init__(self, document, document_id=uuid4().hex):
         document['document_type'] = self.DocumentType.ARTICLE
         super().__init__(document, document_id)
-
-    def input(self):
-        return {
-            'document_id': self.document_id,
-            'document_type': self.document_type.value,
-            'content': self.content,
-            'created_date': self.created_date,
-        }
