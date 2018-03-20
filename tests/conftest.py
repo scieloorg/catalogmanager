f345dda70501b92c45cdf502d82192224b991170
Patricia Morimoto <patriciamorimoto@Patricias-MacBook-Pro.local>
2018-03-20 09:40:23 -0300

    Esboço dos testes funcionais

---
+++ tests/conftest.py
@@ -0,0 +1,30 @@
+import pytest
+from pyramid import testing
+from webtest import TestApp
+
+from catalog_persistence import main
+from catalog_persistence.databases import InMemoryDBManager, DatabaseService
+
+
+@pytest.yield_fixture
+def functional_config(request):
+    yield testing.setUp()
+    testing.tearDown()
+
+
+@pytest.fixture
+def change_service(functional_config):
+    store = InMemoryDBManager('test')
+    return DatabaseService(store, 'test')
+
+
+@pytest.fixture
+def testapp(functional_config):
+    settings = {
+        'couchdb.uri': 'http://localhost:5984',
+        'couchdb.db_name': 'catalog_manager',
+        'couchdb.username': 'admin',
+        'couchdb.password': 'password',
+    }
+    test_app = main(settings)
+    return TestApp(test_app)
