5488bc78063bd121230ec056aeafbcd4ad5be631
robertatakenaka <roberta.takenaka@scielo.org>
2018-04-03 16:10:51 -0300

    tk2 Registrar Artigo + seus ativos digitais
    - Modelo de artigo (Article) e ativos digitais (Asset)
    - Modelo de XML de artigo (ArticleXMLTree) e de ativos digitais (AssetXMLNode)
    - ArticleServices que executa o recebimento de um artigo e seus ativos
    
    Testes:
    - teste de criação de um objeto artigo
    - teste de identificação de href dentro do artigo do XML
    - teste de atualização do href
    - teste de recebimento de XML e seus arquivos relacionados (ativos digitais)
    
    Inclui:
    - 3 Pacotes com um XML + seus ativos
    
    Fixes #2

---
+++ catalogmanager/article_services.py
@@ -0,0 +1,64 @@
+# coding=utf-8
+
+from catalog_persistence.models import (
+        get_record,
+        RecordType,
+    )
+from catalog_persistence.databases import (
+        DatabaseService,
+    )
+from .data_services import DataServices
+from .models.article_model import (
+    Article,
+    File
+)
+
+
+Record = get_record
+
+
+def FileProperties(file):
+    return {
+        'content_size': file.size,
+        'content_type': file.content_type,
+        'file_fullpath': file.file_fullpath,
+        'file_name': file.name,
+        'file_path': file.path,
+    }
+
+
+class ArticleServices:
+
+    def __init__(self, articles_db_manager, changes_db_manager):
+        self.article_data_services = DataServices('articles')
+        self.article_db_service = DatabaseService(
+            articles_db_manager, changes_db_manager)
+
+    def receive_article(self, id, xml, files):
+        article = Article(xml, files)
+        article.id = id
+        article_record = Record(
+            document_id=article.id,
+            content=article.get_record_content(),
+            document_type=RecordType.ARTICLE)
+
+        self.article_db_service.register(
+            article.id, article_record)
+
+        f = File(article.xml_tree.file_fullpath)
+        self.article_db_service.put_attachment(
+                document_id=article.id,
+                file_id=f.name,
+                content=f.content,
+                file_properties=FileProperties(f)
+            )
+
+        if article.assets is not None:
+            for name, asset in article.assets.items():
+                self.article_db_service.put_attachment(
+                        document_id=article.id,
+                        file_id=asset.file.name,
+                        content=asset.file.content,
+                        file_properties=FileProperties(asset.file)
+                    )
+        return self.article_db_service.read(article.id)
---
+++ tests/test_article_services.py
@@ -0,0 +1,62 @@
+import os
+
+from catalog_persistence.databases import (
+    InMemoryDBManager,
+)
+from catalog_persistence.models import (
+    RecordType,
+)
+
+from catalogmanager.article_services import(
+    ArticleServices,
+)
+
+
+def get_article(filename):
+    path = os.path.dirname(filename)
+    files = [path + '/' + item for item in os.listdir(path) if not item.endswith('.xml')]
+    xml_filename = filename
+    return (xml_filename, files)
+
+
+def test_receive_article():
+
+    xml_filename = '/Users/roberta.takenaka/github.com/scieloorg/catalogmanager/packages/0034-8910-rsp-S01518-87872016050006741/0034-8910-rsp-S01518-87872016050006741.xml'
+    xml_filename, files = get_article(xml_filename)
+
+    changes_db_manager = InMemoryDBManager(database_name='changes')
+    articles_db_manager = InMemoryDBManager(database_name='articles')
+
+    article_services = ArticleServices(articles_db_manager, changes_db_manager)
+
+    article_content = {
+        'xml': '0034-8910-rsp-S01518-87872016050006741.xml',
+        'assets': [
+            {
+                'file_href': '0034-8910-rsp-S01518-87872016050006741-gf01.jpg',
+                'file_id': '0034-8910-rsp-S01518-87872016050006741-gf01.jpg'
+            },
+            {
+                'file_href': '0034-8910-rsp-S01518-87872016050006741-gf01-pt.jpg',
+                'file_id': '0034-8910-rsp-S01518-87872016050006741-gf01-pt.jpg'
+            },
+        ]
+    }
+
+    expected = {
+        'attachments': [
+            '0034-8910-rsp-S01518-87872016050006741.xml',
+            '0034-8910-rsp-S01518-87872016050006741-gf01.jpg',
+            '0034-8910-rsp-S01518-87872016050006741-gf01-pt.jpg',
+        ],
+        'content': article_content,
+        'document_type': 'ART',
+        'document_id': 'ID',
+    }
+
+    got = article_services.receive_article('ID', xml_filename, files)
+    if 'created_date' in got.keys():
+        del got['created_date']
+    print(expected)
+    print(got)
+    assert expected == got
