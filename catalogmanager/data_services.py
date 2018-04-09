5e80748f64febe0f3144d2a647c7c6d5d84ae7ca
Patricia Morimoto <patricia.morimoto@scielo.org>
2018-04-09 08:42:09 -0300

    Retirado método não mais necessário do DataService

--- catalogmanager/data_services.py
+++ catalogmanager/data_services.py
@@ -8,7 +8,3 @@ class DataServices:
 
     def location(self, record_id):
         return '/{}/{}'.format(self.name, record_id)
-
-    def get_article_id(self, article_url):
-        _, _, record_id = article_url.split('/')
-        return record_id
