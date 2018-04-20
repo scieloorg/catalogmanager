fc59fe17e289ed01334078836348936c626fea52
Patricia Morimoto <patricia.morimoto@scielo.org>
2018-04-20 09:14:29 -0300

    Correção do bug do href dos ativos digitais no XML
    - Criação de `catalogmanager.set_assets_public_url`
    - Alteração de `Article.get_article_xml` para alterar os hrefs dos
    ativos digitais ao recuperar o artigo XML
    - Correção da property `HRefNode.local_href` para verificação do path
    - Criação de testes unitários para recuperar XML atualizado
    - Ajuste dos testes unitários e funcional

--- catalogmanager/__init__.py
+++ catalogmanager/__init__.py
@@ -1,6 +1,8 @@
 from catalogmanager.article_services import (
     ArticleServices
 )
+from catalogmanager.models.article_model import Article
+from catalogmanager.models.file import File
 from catalog_persistence.databases import CouchDBManager
 
 
@@ -45,3 +47,16 @@ def get_article_file(article_id, db_host, db_port, username, password):
                                             username,
                                             password)
     return article_services.get_article_file(article_id)
+
+
+def set_assets_public_url(article_id, xml_content, assets_filenames,
+                          public_url):
+    article = Article(article_id)
+    xml_file = File("xml_file.xml")
+    xml_file.content = xml_content
+    xml_file.size = len(xml_content)
+    article.xml_file = xml_file
+    for name in article.assets:
+        if name in assets_filenames:
+            article.assets[name].href = public_url.format(article_id, name)
+    return article.xml_tree.content
--- tests/test_functional.py
+++ tests/test_functional.py
@@ -1,15 +1,14 @@
 
 import os
+import io
 import json
 from collections import OrderedDict
 
 import pytest
 import webtest
+from lxml import etree
 
 from catalogmanager_api import main
-from catalogmanager.xml.xml_tree import (
-    XMLTree
-)
 from .conftest import (
     PKG_A,
 )
@@ -58,14 +57,21 @@ def test_add_article_register_change(testapp, setup_couchdb):
     for asset_file in assets_files:
         assert os.path.basename(asset_file) in res_assets
 
-    # Deve ser possível ter uma URL para resgatar o arquivo XML
+    # Deve ser possível ter uma URL para resgatar o arquivo XML e os ativos
+    # digitais
     xml_url = '{}/xml'.format(url)
     result = testapp.get(xml_url)
     assert result.status_code == 200
-    with open(xml_file_path, 'rb') as fb:
-        xml_tree = XMLTree()
-        xml_tree.content = fb.read()
-        assert xml_tree.compare(result.body)
-
-    # Checa se a lista de mudanças trás o registro de mundança do registro do
-    # documento
+    assert result.body is not None
+    xml_tree = etree.parse(io.BytesIO(result.body))
+    xpath = '{http://www.w3.org/1999/xlink}href'
+    xml_nodes = [
+        node.get(xpath)
+        for node in xml_tree.findall('.//*[@{}]'.format(xpath))
+    ]
+    expected_hrefs = [
+        '{}/{}'.format(url, os.path.basename(asset_file))
+        for asset_file in assets_files
+    ]
+    for expected_href in expected_hrefs:
+        assert expected_href in xml_nodes
